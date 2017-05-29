203963933ecbd35dec908e831b5e66fe9854f6cf
adita08 <tavishi@adityas-mbp.home>
2017-05-29 18:50:53 +0100

    add socket.io module

--- server/server.js
+++ server/server.js
@@ -2,15 +2,32 @@
 const express = require('express');
 const path = require('path');
 
+//load socket io library
+const socketIO = require('socket.io');
+const http = require('http');
+
 const publicFolder = path.join(__dirname, '../public');
 var port = process.env.PORT || 3000;
 var app = express();
+//create server using http module
+var server = http.createServer(app);
+//integrate socket.io to application
+var io = socketIO(server);
 
-//set public folder
+//open web socket when client initiates socket request
+io.on('connection',(socket) => {
+  console.log('New Client Connection');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconected');
+  });
+});
+
+//set public folder for app
 app.use(express.static(publicFolder));
 
 //start server
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Node JS start at ${port}`);
 });
 
