253ec8afbc3178f0f640fed6450d2a492fd15b34
adita08 <tavishi@adityas-mbp.home>
2017-06-03 09:47:15 +0100

    basic chat app using jquery

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,6 @@
 //initiate request to server
 var socket = io();
 
-//action on an even 'connect'
-socket.on('connect', function() {
-  console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'sendmessahe@email.com',
-    text: 'Hello, How are you !!!'
-  })
-});
-
 //action on an another event 'disconnect'
 socket.on('disconnect', function() {
   console.log('Disconnected to server')
@@ -18,4 +9,22 @@ socket.on('disconnect', function() {
 //action on event 'NewMessage'
 socket.on('newMessage', function(newMsg) {
   console.log('Recevied new message', newMsg);
+
+  //create new element using jQuery
+  var li = jQuery('<li></li>');
+  //set text of the element list
+  li.text(`${newMsg.from}: ${newMsg.text}`);
+
+  //append list into order list using id
+  jQuery('#messages').append(li);
+});
+
+//action to be performed when user clicks submit button
+jQuery('#message-form').on('submit', function (e){
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'user',
+    text: jQuery('[name=message]').val()
+  });
 });
